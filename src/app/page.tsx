60be4a609f0affcd33528f128269ac60ada33dab
MarcBTHT <marc.berthol29@gmail.com>
2024-04-06 19:06:51 +0200

    feat: :sparkles: form page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 'use client';
 
 import Link from 'next/link';
-import Header from '../components/header'
-import Footer from '../components/footer'
-import form from '../components/form'
+import Form from '../components/form'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='bg-light_blue'>
+    <div className='bg-white'>
+      <Image
+        src="/images/background.png"
+        alt="background"
+        layout='fill'
+      />
       <div className='backdrop-blur-3xl flex flex-col min-h-screen mx-auto' style={{ maxWidth: '1500px' }}>
-        <Header />
         <main className="flex-grow">
           <section className='text-center min-h-screen flex flex-col justify-center relative'>
-            <div className='relative z-10 mx-auto w-2/3'>
-              {/* <form /> */}
-            </div>
+
+              <Form />
+
           </section>
         </main>
-        <Footer />
       </div>
     </div >
   );
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,10 +1,63 @@
 'use client';
 
+import React, { useState } from 'react';
+import { Card, CardHeader, CardBody, CardFooter, Input, Button } from "@nextui-org/react";
+import Link from 'next/link';
 
 export default function Form() {
-    
-    return (
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [bankName, setBankName] = useState('');
 
-        <></>
+    const handleNextClick = () => {
+        // const formDataUser = {
+        //     firstName,
+        //     lastName,
+        //     bankName,
+        // };
+
+        // // Retrieve existing data from localStorage
+        // const existingData = localStorage.getItem('formDataUser');
+        // const DataUser = existingData ? JSON.parse(existingData) : [];
+
+        // // Add the new forms data to the array
+        // DataUser.push(formDataUser);
+
+        // // Save the updated array back to localStorage
+        // localStorage.setItem('DataUser', JSON.stringify(DataUser));
+        // console.log('New formDataUser added', DataUser);
+    };
+
+
+    return (
+        <div className="flex items-center justify-center">
+            <div className="flex flex-col gap-4 w-1/3">
+                <Card className=" p-3 bg-opacity-60 max-w-lg w-full">
+                    <CardHeader className="flex gap-3">
+                        <div className="flex flex-col mx-auto justify-center mt-2">
+                            <p className="mx-auto">Enter your personal information</p>
+                        </div>
+                    </CardHeader>
+                    <CardBody>
+                        <Input type="text" variant="bordered" label="First name" className="my-3"
+                            onChange={(e) => setFirstName(e.target.value)} />
+
+                        <Input type="text" variant="bordered" label="Last name" className="my-3"
+                            onChange={(e) => setLastName(e.target.value)} />
+
+                        <Input type="text" variant="bordered" label="Bank name" className="my-3"
+                            onChange={(e) => setBankName(e.target.value)} />
+                    </CardBody>
+                    <CardFooter className="mb-2 flex justify-center items-center">
+                        <div className="flex gap-4">
+                            <Button className="bg-tiffany_blue" onClick={handleNextClick}>Save</Button>
+                            <Link href="/" className="text-sm text-black flex items-center">
+                                Go back home
+                            </Link>
+                        </div>
+                    </CardFooter>
+                </Card>
+            </div>
+        </div>
     );
 }

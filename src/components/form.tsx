4f026743653ab09699ea0089a13b147c658c7aaf
MarcBTHT <marc.berthol29@gmail.com>
2024-04-07 14:03:51 +0200

    style: :sparkles: Display if successful or not

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -14,25 +14,28 @@ export default function Form() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [bankName, setBankName] = useState('');
+    const [date, setDate] = useState('');
+    const [resultMessage, setResultMessage] = useState('');
+    const [messageType, setMessageType] = useState('none');
 
     function toUint8Array(str: string) {
         return new TextEncoder().encode(str);
     }
-    
+
     // Fonction pour hasher prénom, nom, et nom de banque
     function hashNameBank(firstName: string, lastName: string, bankName: string) {
         // Concaténer les chaînes avec un délimiteur pour assurer l'unicité
         const combinedString = `${firstName}|${lastName}|${bankName}`;
-        
+
         // Convertir la chaîne combinée en Uint8Array
         const combinedUint8Array = toUint8Array(combinedString);
-        
+
         // Hasher avec keccak256
         const hash = keccak256(combinedUint8Array);
-        
+
         return hash;
     }
-    
+
     const handleNextClick = async () => {
         const account = getAccount(config);
         console.log("Account: ", account.address);
@@ -45,9 +48,18 @@ export default function Form() {
                 functionName: 'checkData',
                 args: [account.address, resultHash],
             });
+            if (result) {
+                setResultMessage('Success! Your data matches.'); // Update message on success
+                setMessageType('success');
+            } else {
+                setResultMessage('Failure! Your data does not match.'); // Update message on failure
+                setMessageType('error');
+            }
             console.log(result);
         } catch (error) {
             console.error("An error occurred:", error);
+            setResultMessage('An error occurred.');
+            setMessageType('error');
         }
     };
 
@@ -57,7 +69,7 @@ export default function Form() {
                 <Card className=" p-3 bg-opacity-100 bg-gray-800 max-w-lg w-full">
                     <CardHeader className="flex gap-3">
                         <div className="flex flex-col mx-auto justify-center mt-2">
-                            <p className="mx-auto mb-3 text-xl text-white">Enter your personal information</p>
+                            <p className="mx-auto mb-3 text-2xl text-white">Enter your personal information</p>
                             <ConnectWallet />
                         </div>
                     </CardHeader>
@@ -70,14 +82,22 @@ export default function Form() {
 
                         <Input type="text" variant="bordered" label="Bank name" className="my-3 text-white"
                             onChange={(e) => setBankName(e.target.value)} />
+                        <Input type="date" variant="bordered" label="Date" className="my-3 text-white" defaultValue='2002-01-01'
+                            onChange={(e) => setDate(e.target.value)} />
                     </CardBody>
-                    <CardFooter className="mb-2 flex justify-center items-center">
+                    <CardFooter className="mb-2 flex flex-col justify-center items-center">
                         <div className="flex gap-4">
                             <Button className="bg-tiffany_blue" onPress={handleNextClick}>Save</Button>
                             <Link href="/" className="text-sm text-white flex items-center">
                                 Go back home
                             </Link>
                         </div>
+                        {/* Display the result message */}
+                        {resultMessage && (
+                            <p className={`text-center mt-4 ${messageType === 'success' ? 'text-green-500' : 'text-red-500'}`}>
+                                {resultMessage}
+                            </p>
+                        )}
                     </CardFooter>
                 </Card>
             </div>
